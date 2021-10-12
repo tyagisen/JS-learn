d876b7c229ea7f38027e47db5ac5f32e6f436f4b
tyagisen <tyagisen@gmail.com>
2021-10-12 21:15:50 +0545

    all done

--- day 02 objects and array/index.js
+++ day 02 objects and array/index.js
@@ -34,15 +34,16 @@ function sum(){
 sum() //invoking sum function
 
 
-function subs(firstNum, secondNum){
+function subs(firstNum, secondNum){//here is the parameter
     z = firstNum-secondNum;
     console.log(z);
 };
-subs(20, 2) //Invoking subs function
+subs(20, 2) //Invoking subs function. here is argument.
 
 
-function returnFunc(firstInp, secondInp){
+function returnFunc(firstInp, secondInp){ //Here is the parameter
     actualVal = firstInp*secondInp;
     return actualVal; //It returns to function
 };
-console.log(returnFunc(2,4)); //invoking values and printing the value.
+console.log(returnFunc(2,4)); //invoking values and printing the value. 2 and 4 is argument.
+console.log(returnFunc(9, 7));
