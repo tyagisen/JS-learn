e3bbb5c795ef33dbc928ef417035f53c98d32b42
tyagisen <tyagisen@gmail.com>
2021-10-12 11:02:43 +0545

    Js day 01

---
+++ day 01 variables/index.js
@@ -0,0 +1,35 @@
+let firstName = "tyagi"
+var surName = "sen"
+const pi = 3.1415
+// var and let can be reassigned
+firstName = "samir"
+surName = "aparichit"
+// pi = 3 this line will show an error as identifier is const. value cannt be reassigned.
+console.log(pi)
+console.log(firstName+"")
+console.log(surName)
+
+/*
+Object creation in JS. All Array are object but all object arenot array.
+*/
+console.log()
+console.log()
+let myObj = {
+    firstName: "Im",
+    age : 23,
+    phone: 879328998493
+}
+console.log(typeof(myObj))
+console.log(myObj)
+
+// ways to access data of object
+
+console.log(myObj.firstName)
+console.log(myObj['phone'])
+console.log()
+
+// ways to manupulate data from the object.
+
+myObj.firstName = "bhuwan"
+console.log(myObj.firstName)
+console.log(myObj['firstName'])
